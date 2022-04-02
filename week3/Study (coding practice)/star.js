e7d4063e99026f18d27437e10b71a90ea9d63b14
변재호 <bjho606@naver.com>
2022-04-02 22:36:32 +0900

    백준 별찍기 연습

---
+++ week3/Study (coding practice)/star.js
@@ -0,0 +1,40 @@
+// 2438
+// star = ""
+// for (let i=0; i<5; i++){
+//   star += "*";
+//   console.log(star);
+// }
+
+// 2439
+// let star = '';
+// for (let i=0; i<5; i++){
+//   star += '*';
+//   let blank = '';
+//   for (let j=4; j > i; j--){
+//     blank += ' ';
+//   }
+  
+//   console.log(blank + star);
+// }
+
+// 2440
+// for (let i=5; i>0; i--){
+//     let star = '*'.repeat(i);
+    
+//     console.log(star);
+//   }
+
+// 2442
+// let star = '';
+// for(let i=0; i<5; i++){
+//   let blank1 = '';
+//   for(let j=4; j>i; j--){
+//     blank1 += ' ';
+//   }
+//   star = '*'.repeat(i*2 + 1);
+//   let blank2 = '';
+//   for (let j=4; j>i; j--){
+//     blank2 += ' ';
+//   }
+//   console.log(blank1 + star + blank2);
+// }
